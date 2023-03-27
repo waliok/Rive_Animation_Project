eecac54d8b7e34016b0f5934c817ee2bddcbb5c8
Valentyn <waliok2292@gmail.com>
2023-03-28 00:57:32 +0200

    Initial Commit

---
+++ Rive_Animation/Rive_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rive_AnimationApp.swift
+//  Rive_Animation
+//
+//  Created by Waliok on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Rive_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
